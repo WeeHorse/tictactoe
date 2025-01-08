5e3612b1639c2aa5f3c588c551913fb9b69e7a73
Benjamin Berglund <benjaminberglund@gmail.com>
2025-01-08 14:11:05 +0100

    added database and example

--- TicTacToe.cs
+++ TicTacToe.cs
@@ -1,21 +1,38 @@
+using Npgsql;
 using TicTacToe.Records;
 
 namespace TicTacToe;
 
 public class TicTacToeGame
 {
+    // initialize database
+    Database database = new();
+    private NpgsqlDataSource db;
+    
     // Routes are defined and registered for listening for network requests
     // in the constructor below
     // and then in turn processed by methods as called from the routes definitions 
     public TicTacToeGame (WebApplication app)
     {
+        // Get database connection
+        db = database.Connection();
+        
         // Map incomming request for current game data
         app.MapGet("/api/current-game/{id}", GetCurrentGame);
     }
 
-    Game GetCurrentGame(int id)
+    async Task<Game>? GetCurrentGame(int id)
     {
-        return new Game(1);
+        await using var cmd = db.CreateCommand("SELECT * FROM games WHERE id = $1");
+        cmd.Parameters.AddWithValue(id);
+        await using (var reader = await cmd.ExecuteReaderAsync())
+        {
+            while (await reader.ReadAsync())
+            {
+                return new Game(reader.GetInt32(0),reader.GetInt32(1),reader.GetInt32(2),reader.GetString(3));
+            }
+        }
+        return null;
     }
 
     
